c655286ed5a6eadbdd7615e94b97681000543d83
Alberto <albertodelcru@gmail.com>
2016-01-29 22:08:32 +0100

    Script camera and fadeOut

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour
+{
+    public float speed = 2;
+    public float maxLeft, maxRight;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 position = Input.mousePosition;
+        if ((Screen.width - position.x) < 5 && this.gameObject.transform.position.x < maxRight)
+        {
+            this.gameObject.transform.position += Vector3.right * speed * Time.deltaTime;
+            Debug.Log("right");
+        }
+        else if (position.x < 5 && this.gameObject.transform.position.x >= maxLeft)
+        {
+            this.gameObject.transform.position += Vector3.left * speed * Time.deltaTime;
+            Debug.Log("left");
+        }
+    }
+}
---
+++ Assets/Scripts/FadeOut.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeOut : MonoBehaviour
+{
+    public float max, min, time;
+    private Renderer rend;
+    // Use this for initialization
+    void Start()
+    {
+        rend = this.GetComponent<SpriteRenderer>();
+        fade();
+    }
+
+    void Update()
+    {
+        //fade();
+    }
+
+    void fade()
+    {
+        /*Color color = rend.material.color;
+        color.a = new Color(1f, 1f, 1f, Mathf.SmoothStep(max, min, time));*/
+        Color col = new Color(1f, 1f, 1f, Mathf.SmoothStep(max, min, time));
+        rend.material.color = col;
+
+    }
+}
